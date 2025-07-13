e857c56be810ad387508b41da47a86b439f5533e
Ancelia Mathias Patrao <55922585+anceliaa06@users.noreply.github.com>
2025-07-13 20:25:47 +0530

    Create Person.cs

---
+++ Person.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonAssign
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Address { get; set;  }
+        public DateTime Dob { get; set; }
+
+        public Person(int id, string address, DateTime dob)
+        {
+            Id = id;
+            Address = address;
+            Dob = dob;
+        }
+    }
+}
